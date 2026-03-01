833a173a6cd5a720845e899a0a9edb46481bbcd2
Raveen Abeydeera <sandinu15@gmail.com>
2026-03-01 18:16:50 +0530

    Add files via upload

--- server/routes/sightingRoutes.js
+++ server/routes/sightingRoutes.js
@@ -17,7 +17,7 @@ cloudinary.config({
 // 2. Configure Storage Engine (v2.2.1 syntax)
 const storage = cloudinaryStorage({
   cloudinary: cloudinary,
-  folder: 'wildroute_sightings',
+  folder: 'dmewqz26w',
   allowedFormats: ['jpg', 'png', 'jpeg', 'gif'],
 });
 
